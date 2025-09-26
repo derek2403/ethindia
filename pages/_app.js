0f388c6b1829749df9758e21e6eda89454dd6e78
Cedric <cedricctf11a@gmail.com>
2025-09-26 23:39:07 +0800

    configured desired chains and generated the required connectors

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,13 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 
+const config = getDefaultConfig({
+  appName: 'ethindia',
+  projectId: '',
+  chains: [mainnet, polygon, optimism, arbitrum, base, sepolia],
+  ssr: true,
+});
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
