1a2a42c43619f29a5d171f9852ba02b9ca9542b4
Cedric <cedricctf11a@gmail.com>
2025-09-26 23:07:21 +0530

    linked token balance component with atomic transfer component

--- pages/transfer.js
+++ pages/transfer.js
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import { Header } from '../components/Header';
 import AtomicTransfer from '../components/AtomicTransfer';
-
+import TokenBalance from '../components/TokenBalance';
 
 export default function Transfer() {
+  const [transferAmounts, setTransferAmounts] = useState({});
+
   return (
     <div>
       <Header />
       <main className="p-4">
-        <AtomicTransfer />
+        <TokenBalance 
+          transferAmounts={transferAmounts}
+          setTransferAmounts={setTransferAmounts}
+        />
+        <AtomicTransfer 
+          transferAmounts={transferAmounts}
+        />
       </main>
     </div>
   );
