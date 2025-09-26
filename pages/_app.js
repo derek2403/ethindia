488ed66d3d3a11842becdc4e6698992ffd2b18ab
Cedric <cedricctf11a@gmail.com>
2025-09-26 23:37:37 +0800

    import rainbowkit,wagmi and tan stack query

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,22 @@
 import "@/styles/globals.css";
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
