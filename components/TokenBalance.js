193dc3beb2a36faa02285275d8d3170047a261a0
Cedric <cedricctf11a@gmail.com>
2025-09-28 05:00:51 +0530

    removed alert

--- components/TokenBalance.js
+++ components/TokenBalance.js
@@ -385,11 +385,6 @@ export default function TokenBalance({ transferAmounts = {}, setTransferAmounts,
               </div>
             </div>
             
-            {currentTotalUSD >= maxPaymentAmount && (
-              <div className="text-sm text-red-400 bg-red-500/10 border border-red-500/20 rounded-lg p-2">
-                ⚠️ Payment limit reached. Remove some tokens to add more.
-              </div>
-            )}
           </div>
         )}
 
