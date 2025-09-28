fc425a7ae038aeae1bb2ebde93ec11324203ce35
derek2403 <derekliew0@gmail.com>
2025-09-28 05:34:12 +0530

    added shell cmds

---
+++ Hedera-OP/my-lz-oapp/hedera.sh
@@ -0,0 +1,6 @@
+npx hardhat lz:oft:send \
+  --src-eid 40161 \
+  --dst-eid 40285 \
+  --amount 100 \
+  --to 0x8fdd8FF672BEf99e33A1F821ECDC57571391e9B5 \
+  --network sepolia
---
+++ Hedera-OP/my-lz-oapp/sepolia.sh
@@ -0,0 +1,6 @@
+npx hardhat lz:oft:send \
+  --src-eid 40285 \
+  --dst-eid 40161 \
+  --amount 50 \
+  --to 0x8fdd8FF672BEf99e33A1F821ECDC57571391e9B5 \
+  --network hedera-testnet
